57b2a3f2c60e485b7980cd73bd3a1f7fa17c2eac
Vladimir Varankin <vladimir@varank.in>
2019-11-27 17:42:06 +0100

    pkg/profefe: unify data format returned from ReplyJSON and ReplyError

--- pkg/profefe/reply_test.go
+++ pkg/profefe/reply_test.go
@@ -13,6 +13,22 @@ import (
 	"golang.org/x/xerrors"
 )
 
+func TestReplyJSON(t *testing.T) {
+	w := httptest.NewRecorder()
+	body := map[string]interface{}{"foo": "bar"}
+
+	ReplyJSON(w, body)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "application/json", w.Header().Get("content-type"))
+
+	var resp map[string]interface{}
+	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
+
+	assert.EqualValues(t, http.StatusOK, resp["code"])
+	assert.Equal(t, body, resp["body"])
+}
+
 func TestReplyError(t *testing.T) {
 	testRawErr := errors.New("unexpected error")
 
@@ -32,12 +48,12 @@ func TestReplyError(t *testing.T) {
 			"bad request",
 		},
 		{
-			xerrors.Errorf("unexpted error: %w", StatusError(http.StatusBadRequest, "bad request", nil)),
+			xerrors.Errorf("unexpected error: %w", StatusError(http.StatusBadRequest, "bad request", nil)),
 			http.StatusBadRequest,
 			"bad request",
 		},
 		{
-			xerrors.Errorf("unexpted error: %w", testRawErr),
+			xerrors.Errorf("unexpected error: %w", testRawErr),
 			http.StatusInternalServerError,
 			"internal error",
 		},
@@ -54,11 +70,11 @@ func TestReplyError(t *testing.T) {
 	}
 
 	for n, tc := range cases {
-		t.Run(fmt.Sprintf("case %d", n), func(t *testing.T) {
+		t.Run(fmt.Sprintf("case=%d", n), func(t *testing.T) {
 			w := httptest.NewRecorder()
 			ReplyError(w, tc.err)
-			require.Equal(t, tc.wantCode, w.Code)
 
+			require.Equal(t, tc.wantCode, w.Code)
 			assert.Equal(t, "application/json", w.Header().Get("content-type"))
 
 			var resp map[string]interface{}
