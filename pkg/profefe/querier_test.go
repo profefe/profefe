41e7b8b39af60179af8b64e0551bb48d172db6c0
Vladimir Varankin <vladimir@varank.in>
2020-02-15 23:25:48 +0100

    pkg/profefe: make sure querier closes profiles list on bailing out

--- pkg/profefe/querier_test.go
+++ pkg/profefe/querier_test.go
@@ -15,12 +15,13 @@ import (
 )
 
 func TestQuerier_FindMergeProfileTo_contextCancelled(t *testing.T) {
+	list := &unboundProfileList{}
 	sr := &storage.MockReader{
 		FindProfileIDsMock: func(ctx context.Context, _ *storage.FindProfilesParams) ([]profile.ID, error) {
 			return []profile.ID{profile.NewID()}, nil
 		},
 		ListProfilesMock: func(ctx context.Context, _ []profile.ID) (storage.ProfileList, error) {
-			return &unboundProfileList{}, nil
+			return list, nil
 		},
 	}
 
@@ -37,14 +38,22 @@ func TestQuerier_FindMergeProfileTo_contextCancelled(t *testing.T) {
 	}
 	err := querier.FindMergeProfileTo(ctx, ioutil.Discard, params)
 	assert.Equal(t, context.Canceled, err)
+
+	assert.True(t, list.closed, "profile list must be closed")
 }
 
 // unbound profile list whose Next method always returns true
-type unboundProfileList struct{}
+type unboundProfileList struct {
+	closed bool
+}
 
 func (pl *unboundProfileList) Next() bool {
 	return true
 }
 
+func (pl *unboundProfileList) Close() error {
+	pl.closed = true
+	return nil
+}
+
 func (pl *unboundProfileList) Profile() (p *pprofProfile.Profile, err error) { return }
-func (pl *unboundProfileList) Close() (err error)                            { return }
