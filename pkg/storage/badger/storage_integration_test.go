d770f7c8b849379a8324a1ec77e99c03d2824696
Vladimir Varankin <vladimir@varank.in>
2020-04-25 20:40:23 +0200

    sotragetest: integration tests to use testify/suite

--- pkg/storage/badger/storage_integration_test.go
+++ pkg/storage/badger/storage_integration_test.go
@@ -10,6 +10,7 @@ import (
 	storageBadger "github.com/profefe/profefe/pkg/storage/badger"
 	"github.com/profefe/profefe/pkg/storage/storagetest"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 )
@@ -18,17 +19,34 @@ func TestStorage(t *testing.T) {
 	dbPath, err := ioutil.TempDir("", "badger")
 	require.NoError(t, err)
 
+	t.Cleanup(func() {
+		os.RemoveAll(dbPath)
+	})
+
 	opts := badger.DefaultOptions(dbPath)
 	db, err := badger.Open(opts)
 	require.NoError(t, err)
 
 	t.Cleanup(func() {
 		db.Close()
-		os.RemoveAll(dbPath)
 	})
 
 	testLogger := zaptest.NewLogger(t, zaptest.Level(zapcore.FatalLevel))
 	st := storageBadger.NewStorage(log.New(testLogger), db, 0)
 
-	storagetest.RunTestSuite(t, st)
+	t.Run("Reader", func(t *testing.T) {
+		ts := &storagetest.ReaderTestSuite{
+			Reader: st,
+			Writer: st,
+		}
+		suite.Run(t, ts)
+	})
+
+	t.Run("Writer", func(t *testing.T) {
+		ts := &storagetest.WriterTestSuite{
+			Reader: st,
+			Writer: st,
+		}
+		suite.Run(t, ts)
+	})
 }
--- pkg/storage/s3/storage_integration_test.go
+++ pkg/storage/s3/storage_integration_test.go
@@ -16,6 +16,7 @@ import (
 	storageS3 "github.com/profefe/profefe/pkg/storage/s3"
 	"github.com/profefe/profefe/pkg/storage/storagetest"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 )
@@ -46,7 +47,21 @@ func TestStorage(t *testing.T) {
 	testLogger := zaptest.NewLogger(t, zaptest.Level(zapcore.FatalLevel))
 	st := storageS3.NewStorage(log.New(testLogger), svc, s3Bucket)
 
-	storagetest.RunTestSuite(t, st)
+	t.Run("Reader", func(t *testing.T) {
+		ts := &storagetest.ReaderTestSuite{
+			Reader: st,
+			Writer: st,
+		}
+		suite.Run(t, ts)
+	})
+
+	t.Run("Writer", func(t *testing.T) {
+		ts := &storagetest.WriterTestSuite{
+			Reader: st,
+			Writer: st,
+		}
+		suite.Run(t, ts)
+	})
 }
 
 func setupS3Bucket(t *testing.T, svc *s3.S3, bucket string) {
