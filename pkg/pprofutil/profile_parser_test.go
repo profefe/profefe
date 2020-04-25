18a0cc4ec5be20b650071091fcc02fa782133f0d
Vladimir Varankin <vladimir@varank.in>
2020-04-25 17:31:24 +0200

    pkg/pprofutil: return parser error on malformed profile

--- pkg/pprofutil/profile_parser_test.go
+++ pkg/pprofutil/profile_parser_test.go
@@ -1,6 +1,7 @@
 package pprofutil
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -22,6 +23,27 @@ func TestProfileParser(t *testing.T) {
 		require.NoError(t, err)
 
 		require.True(t, ProfilesEqual(prof1, prof2))
+	})
+
+	t.Run("ParseProfile/pprof=malformed", func(t *testing.T) {
+		parser := NewProfileParser([]byte("not a pprof"))
+		_, err := parser.ParseProfile()
+		require.Error(t, err)
+
+		var perr *ProfileParserError
+		require.True(t, errors.As(err, &perr), "got parsing error of type %T (%q)", err, err)
+	})
+
+	t.Run("ParseProfile/pprof=empty", func(t *testing.T) {
+		data, err := ioutil.ReadFile("../../testdata/collector_cpu_no-samples.prof")
+		require.NoError(t, err)
+
+		parser := NewProfileParser(data)
+		_, err = parser.ParseProfile()
+		require.Error(t, err)
+
+		var perr *ProfileParserError
+		require.True(t, errors.As(err, &perr), "got parsing error of type %T (%q)", err, err)
 	})
 
 	t.Run("Reader", func(t *testing.T) {
